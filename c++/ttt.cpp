8e760fe3df9ba73a8977bd4a6b315baf626fb4e9
eli-front <elifront23@gmail.com>
2020-08-17 17:45:15 -0500

    C++ formatting update

--- c++/ttt.cpp
+++ c++/ttt.cpp
@@ -1,44 +1,43 @@
 // Copyright (c) 2020 Eli B. Front. All rights reserved.
 
+#include <stdlib.h>
 #include <iostream>
 #include <string>
-#include <stdlib.h>
 
-using namespace std;
-
-string board[3][3] = {{" ", " ", " "}, {" ", " ", " "}, {" ", " ", " "}};
-string currentPlayer = "x";
-
-bool isnumeric(string& s) {
-  string::const_iterator it = s.begin();
+// Checks if a string is a number
+bool isnumeric(const std::string& s) {
+  std::string::const_iterator it = s.begin();
   while (it != s.end() && isdigit(*it)) ++it;
   return !s.empty() && it == s.end();
-
 }
 
-void printBoard() {
-  cout << "\x1B[2J\x1B[H";
+// Prints the current board and clears the previous board
+void printBoard(std::string board[3][3]) {
+  std::cout << "\x1B[2J\x1B[H";
   for (int i = 0; i < 3; i++) {
-    string line = " " + board[i][0] + " | " + board[i][1] + " | " + board[i][2];
-    cout << line << endl;
+    std::string line =
+      " " + board[i][0] + " | " + board[i][1] + " | " + board[i][2];
+    std::cout << line << std::endl;
     if (i < 2) {
-      cout << "___ ___ ___ " << endl;
+      std::cout << "___ ___ ___ " << std::endl;
     }
   }
 }
 
-bool equalArray(string a[]) {
+// Checks if all elements in an array are equal
+bool equalArray(std::string a[]) {
   return (a[0] == a[1] && a[1] == a[2]);
 }
 
-string gameOver() {
+// If there is a winner it will return that player
+std::string gameOver(std::string board[3][3]) {
   for (int i = 0; i < 3; i++) {
     // horizontal
     if ((equalArray(board[i])) && (board[i][0] != " ")) {
       return board[i][0];
     }
     // vertical
-    string vertBoard[3];
+    std::string vertBoard[3];
     for (int j = 0; j < 3; j++) vertBoard[j] = board[j][i];
 
     if ((equalArray(vertBoard)) && (vertBoard[0] != " ")) {
@@ -46,18 +45,20 @@ string gameOver() {
     }
   }
 
-  string center = board[1][1];
-
+  std::string center = board[1][1];
+  // diagonal
   if (center != " ") {
-    // diag
-    if ((board[0][0] == center && center == board[2][2]) || (board[0][2] == center && center == board[2][0])) {
+    const bool leftRight = (board[0][0] == center && center == board[2][2]);
+    const bool rightLeft = (board[0][2] == center && center == board[2][0]);
+    if (leftRight || rightLeft) {
       return center;
     }
   }
+  // The game is not over
   return " ";
 }
 
-bool boardFull() {
+bool boardFull(std::string board[3][3]) {
   for (int i = 0; i < 3; i++) {
     for (int j = 0; j < 3; j++) {
       if (board[i][j] == " ") return false;
@@ -67,52 +68,66 @@ bool boardFull() {
 }
 
 int main() {
+  std::string board[3][3] = {{" ", " ", " "}, {" ", " ", " "}, {" ", " ", " "}};
+  std::string currentPlayer = "x";
 
-  printBoard();
+  printBoard(board);
 
-  while (gameOver() == " ") {
-    if (boardFull()) {
-      cout << "cat's Game" << endl;
+  std::string output = "";
+  while (gameOver(board) == " ") {
+    if (boardFull(board)) {
+      std::cout << "cat's Game" << std::endl;
       break;
     }
 
-    string x;
-    string y;
+    std::string x;
+    std::string y;
 
-    cout << "type x of your move: ";
+    std::cout << "type x of your move: ";
 
-    cin >> x;
+    std::cin >> x;
 
-    cout << "type y of your move: ";
+    std::cout << "type y of your move: ";
 
-    cin >> y;
-    printBoard();
-
+    std::cin >> y;
+    printBoard(board);
+    // Checks the size of the inputs (should be 1)
     if (x.size() == 1 && y.size() == 1) {
+      // Check if the inputs are numbers
       if (isnumeric(x) && isnumeric(y)) {
+        // Converts the inputs to numbers
         int xNum = atoi(x.c_str());
         int yNum = atoi(y.c_str());
+        // Checks if the inputs fall in the correct range
         if (xNum >= 0 && xNum < 3 && yNum >= 0 && yNum < 3) {
+          // Checks if the input spot is empty
           if (board[yNum][xNum] == " ") {
+            // Sets the board spot to the currentPlayer
             board[yNum][xNum] = currentPlayer;
             currentPlayer = (currentPlayer == "x") ? "o" : "x";
-            printBoard();
+            printBoard(board);
           } else {
-            cout << "this spot is already taken" << endl;
+            output = "this spot is already taken";
           }
         } else {
-          cout << "the inputs must follow these constraints: 0 <= input < 3" << endl;
+          output = "the inputs must follow these constraints: 0 <= input < 3";
         }
       } else {
-        cout << "at least one of the inputs were not integers" << endl;
+        output = "at least one of the inputs were not integers";
       }
     } else {
-      cout << "your input was either too long or short, make sure it has correct formatting\n ex ->1" << endl;
+      output = "your input was either too long or short\n ex ->1";
     }
   }
-  string winner = gameOver();
+  // Outputs an output if it exists
+  if (output != "") {
+    std::cout << output << std::endl;
+  }
+  // Winner of the game
+  std::string winner = gameOver(board);
+  // Checks if it was a cat's game
   if (winner != " ")
-    cout << winner << " wins!" << endl;
+    std::cout << winner << " wins!" << std::endl;
 
   return 0;
 }
